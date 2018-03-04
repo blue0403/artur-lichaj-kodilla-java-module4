db246e8e2a19099f3fd40fd10e7d4849230f8637
blue0403 <artur.lichaj@gmail.com>
2018-03-04 13:48:03 +0100

    First Commit

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.kodillacourse;
+
+public class Calculator {
+    private int addAToB(int a, int b) {
+        return a + b;
+    }
+    private int substractAFromB(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int resultOfAdding = calculator.addAToB(25, 24);
+        int resultOfSubtracting = calculator.substractAFromB(28, 149);
+
+        System.out.println("Result of adding: " + resultOfAdding);
+        System.out.println("Result of subtracting: " + resultOfSubtracting);
+    }
+}
