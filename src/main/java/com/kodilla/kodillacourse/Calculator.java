fc2706d2aa4254449df4c52a9fda061e1b334934
blue0403 <artur.lichaj@gmail.com>
2018-03-14 23:02:07 +0100

    First Commit

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -4,14 +4,14 @@ public class Calculator {
     private int addAToB(int a, int b) {
         return a + b;
     }
-    private int substractAFromB(int a, int b) {
+    private int subtractAFromB(int a, int b) {
         return a - b;
     }
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         int resultOfAdding = calculator.addAToB(25, 24);
-        int resultOfSubtracting = calculator.substractAFromB(28, 149);
+        int resultOfSubtracting = calculator.subtractAFromB(28, 149);
 
         System.out.println("Result of adding: " + resultOfAdding);
         System.out.println("Result of subtracting: " + resultOfSubtracting);
